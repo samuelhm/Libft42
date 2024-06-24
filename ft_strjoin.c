4a41e4b10b5b0c72a52ba2ccff759cd01e7094d1
Samuel Hurtado Marin <shurtado@cbr8s4.42barcelona.com>
2024-06-24 21:32:34 +0200

    ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,25 @@
 /*   By: shurtado <shurtado@student.42barcelona.com +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 13:11:15 by shurtado          #+#    #+#             */
-/*   Updated: 2024/01/14 10:10:59 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/06/24 21:32:11 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int	size;
+
+	size = ft_strlen(s1);
+	size += ft_strlen(s2);
+	size++;
+	str = malloc(sizeof(char) * size);
+	if (!str)
+		return (NULL);
+	ft_bzero(str, size);
+	ft_strlcpy(str, s1, size);
+	ft_strlcat(str, s2, size);
+	return (str);
+}
