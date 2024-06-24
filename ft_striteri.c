7cb62c614cdc7ced21924379fd7b01551069aea4
Samuel Hurtado Marin <shurtado@cbr8s4.42barcelona.com>
2024-06-25 01:20:50 +0200

    ft_iteri added

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,8 +6,22 @@
 /*   By: shurtado <shurtado@student.42barcelona.com +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 13:12:54 by shurtado          #+#    #+#             */
-/*   Updated: 2024/01/14 10:10:59 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/06/25 01:20:23 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
