0df9526d10bbd352ab189361c55897dbee633f0f
Samuel Hurtado Marin <shurtado@cbr6s6.42barcelona.com>
2024-06-26 18:55:47 +0200

    Added ft_lstdelone, fixed norminette got lstmap

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/26 18:08:37 by shurtado          #+#    #+#             */
-/*   Updated: 2024/06/26 18:55:11 by shurtado         ###   ########.fr       */
+/*   Created: 2024/06/26 18:50:20 by shurtado          #+#    #+#             */
+/*   Updated: 2024/06/26 18:52:35 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list	*newlist;
-
-	if (!lst || !f || !del)
-		return (NULL);
-	newlist = NULL;
-	while (lst)
-	{
-		if (!newlist)
-			newlist = ft_lstnew(f(lst->content));
-		else
-			ft_lstadd_back(&lst, ft_lstnew(f(lst->content)));
-	}
-	return (newlist);
+	del(lst->content);
+	free(lst);
 }
