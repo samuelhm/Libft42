f9eeb86dcdf589bcaa119bb629c6b271d5ff19c7
Samuel Hurtado <lnx.wifi@gmail.com>
2024-11-03 18:59:36 +0100

    Fixed return value on ft_strcmp

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 18:44:14 by shurtado          #+#    #+#             */
-/*   Updated: 2024/11/03 18:55:55 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/11/03 18:59:09 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 	i = 0;
 	if (!s1 || !s2)
-		return ((unsigned char)s1 - (unsigned char)s2);
+		return (s1 - s2);
 	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i] || s1[i] == 0 || s2[i] == 0)
